9a5f28b1c567a1f36fac625228312dbc0ba4eed4
alisonthemonster <alison.wyllie@gmail.com>
2018-12-22 11:26:50 -0800

    Create skeleton unit tests

---
+++ app/src/test/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryViewModelTest.kt
@@ -0,0 +1,13 @@
+package journal.gratitude.com.gratitudejournal.ui.entry
+
+import org.junit.Test
+
+class EntryViewModelTest {
+
+    @Test
+    fun entryViewModel_sampleTest() {
+        EntryViewModel()
+
+        assert(true)
+    }
+}
---
+++ app/src/test/java/journal/gratitude/com/gratitudejournal/ui/timeline/TimelineViewModelTest.kt
@@ -0,0 +1,13 @@
+package journal.gratitude.com.gratitudejournal.ui.timeline
+
+import org.junit.Test
+
+class TimelineViewModelTest {
+
+    @Test
+    fun timelineViewModel_sampleTest() {
+        TimelineViewModel()
+
+        assert(true)
+    }
+}
