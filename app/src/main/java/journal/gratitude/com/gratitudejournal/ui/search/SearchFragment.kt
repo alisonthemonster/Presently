1a4c054ff0e01c29983275fcf2e6704884892516
alisonthemonster <alison.wyllie@gmail.com>
2019-04-25 14:36:35 -0700

    Listen for keyboard changes using RxTextView

--- app/src/main/java/journal/gratitude/com/gratitudejournal/ui/search/SearchFragment.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/ui/search/SearchFragment.kt
@@ -17,6 +17,8 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionInflater
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.disposables.CompositeDisposable
 import journal.gratitude.com.gratitudejournal.R
 import journal.gratitude.com.gratitudejournal.databinding.SearchFragmentBinding
 import journal.gratitude.com.gratitudejournal.repository.EntryRepository
@@ -25,7 +27,7 @@ import journal.gratitude.com.gratitudejournal.ui.entry.EntryFragment
 import journal.gratitude.com.gratitudejournal.ui.timeline.TimelineAdapter
 import kotlinx.android.synthetic.main.search_fragment.*
 import org.threeten.bp.LocalDate
-import android.view.WindowManager
+import java.util.concurrent.TimeUnit
 
 
 class SearchFragment : Fragment() {
@@ -35,6 +37,8 @@ class SearchFragment : Fragment() {
     }
     private lateinit var binding: SearchFragmentBinding
     private lateinit var viewModel: SearchViewModel
+
+    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,10 +68,18 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val obs = RxTextView.textChanges(search_text)
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .map<Any> { charSequence -> charSequence.toString() }
+            .subscribe {
+                search(it as String)
+            }
+        disposables.add(obs)
+
         search_text.setOnEditorActionListener(object : TextView.OnEditorActionListener {
             override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    search(v.text.toString())
+                    dismissKeyboard()
                     return true
                 }
                 return false
@@ -75,7 +87,7 @@ class SearchFragment : Fragment() {
         })
 
         search_icon.setOnClickListener {
-           search(search_text.text.toString())
+            dismissKeyboard()
         }
 
         back_icon.setOnClickListener {
@@ -104,13 +116,25 @@ class SearchFragment : Fragment() {
         })
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        disposables.clear() // Using clear will clear all, but can accept new disposable
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.dispose() // Using dispose will clear all and set isDisposed = true, so it will not accept any new disposable
+    }
+
     private fun search(query: String) {
         if (query.isNotEmpty()) {
-            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
-            imm?.hideSoftInputFromWindow(search_text.windowToken, 0)
-
             viewModel.search(query)
         }
+    }
+
+    private fun dismissKeyboard() {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(search_text.windowToken, 0)
     }
 
 
