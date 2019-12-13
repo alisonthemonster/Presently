9b59245b1c4c552c1b8d97eb59780f0445e4832f
alisonthemonster <alison.wyllie@gmail.com>
2019-12-13 11:29:51 -0800

    Get first test to pass!

--- app/src/androidTest/java/journal/gratitude/com/gratitudejournal/TimelineInstrumentedTest.kt
+++ app/src/androidTest/java/journal/gratitude/com/gratitudejournal/TimelineInstrumentedTest.kt
@@ -2,28 +2,23 @@ package journal.gratitude.com.gratitudejournal
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import journal.gratitude.com.gratitudejournal.ui.timeline.TimelineFragment
+import journal.gratitude.com.gratitudejournal.util.RecyclerViewItemCountAssertion.Companion.withItemCount
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-//@RunWith(AndroidJUnit4::class)
-//class TimelineInstrumentedTest {
-//
-//    @Test
-//    fun timelineFragment_showsEntries() {
-//        launchFragmentInContainer<TimelineFragment>()
-//
-//        onView(withId(R.id.title)).check(matches(withText("Presently")))
-//    }
-//
-//}
+@RunWith(AndroidJUnit4::class)
+class TimelineInstrumentedTest {
+
+    @Test
+    fun timelineFragment_showsEntries() {
+        launchFragmentInContainer<TimelineFragment>(
+            themeResId = R.style.AppTheme)
+
+        onView(withId(R.id.timeline_recycler_view)).check(withItemCount(2))
+    }
+
+}
 
