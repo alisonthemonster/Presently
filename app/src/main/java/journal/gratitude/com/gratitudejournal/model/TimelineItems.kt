5947f028c8511044a6247de71ea046043c4bec2d
alisonthemonster <alison.wyllie@gmail.com>
2020-08-08 14:18:38 -0700

    Update milestones

--- app/src/main/java/journal/gratitude/com/gratitudejournal/model/TimelineItems.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/model/TimelineItems.kt
@@ -18,7 +18,7 @@ data class Milestone(val number: Int,
                      val numString: String): TimelineItem() {
 
     companion object {
-        val milestones = arrayOf(5, 10, 25, 50, 100, 150, 200, 250, 300, 365, 400, 450, 500, 550)
+        val milestones = setOf(5, 10, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 365, 400, 425, 450, 475, 500, 525, 550, 575, 600)
 
         fun create(number: Int): Milestone {
             require(milestones.contains(number))
