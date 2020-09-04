e108d8b89743d4b30bc5e573e34ab6b7dc64c66f
alisonthemonster <alison.wyllie@gmail.com>
2020-09-04 13:49:45 -0700

    Move text styling to UI module and stop using Calligraphy and commit fake fonts

--- app/src/main/java/journal/gratitude/com/gratitudejournal/GratitudeApplication.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/GratitudeApplication.kt
@@ -5,9 +5,6 @@ import androidx.work.WorkManager
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import io.github.inflationx.calligraphy3.CalligraphyConfig
-import io.github.inflationx.calligraphy3.CalligraphyInterceptor
-import io.github.inflationx.viewpump.ViewPump
 import journal.gratitude.com.gratitudejournal.di.DaggerApplicationComponent
 import journal.gratitude.com.gratitudejournal.di.DaggerAwareWorkerFactory
 import javax.inject.Inject
@@ -24,17 +21,6 @@ open class GratitudeApplication: DaggerApplication() {
         configureWorkManager()
 
         AndroidThreeTen.init(this)
-
-        ViewPump.init(
-            ViewPump.builder()
-                .addInterceptor(
-                    CalligraphyInterceptor(
-                        CalligraphyConfig.Builder()
-                            .setDefaultFontPath("fonts/Larsseit-Medium.ttf")
-                            .setFontAttrId(R.attr.fontPath)
-                            .build())
-                )
-                .build())
     }
 
     @Inject
