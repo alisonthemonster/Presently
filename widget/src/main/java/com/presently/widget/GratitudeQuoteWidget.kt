3b0d32e0ef6695ec958ad7e8db36e165e46ea773
alisonthemonster <alison.wyllie@gmail.com>
2021-02-15 14:36:30 -0800

    Move string resources to UI module, create widget module, and first iteration of a quote widget

---
+++ widget/src/main/java/com/presently/widget/GratitudeQuoteWidget.kt
@@ -0,0 +1,44 @@
+package com.presently.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class GratitudeQuoteWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.resources.getStringArray(R.array.inspirations).random()
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.gratitude_quote_widget)
+    views.setTextViewText(R.id.quote_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
