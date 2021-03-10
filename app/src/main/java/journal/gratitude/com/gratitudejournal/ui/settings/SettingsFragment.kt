7755f76403efb278d0550f18a8f7141b0c1d5703
alisonthemonster <alison.wyllie@gmail.com>
2021-03-09 20:03:19 -0800

    Clean up

--- app/src/main/java/journal/gratitude/com/gratitudejournal/ui/settings/SettingsFragment.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/ui/settings/SettingsFragment.kt
@@ -7,7 +7,6 @@ import android.content.SharedPreferences
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import android.widget.Toast
