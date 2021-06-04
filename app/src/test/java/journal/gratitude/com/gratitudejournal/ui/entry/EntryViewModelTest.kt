4009b4975cf4fc0e227f44a7e1589288e70bb2cb
alisonthemonster <alison.wyllie@gmail.com>
2021-06-03 19:21:05 -0700

    Comment out test to determine if coverage is actually working for app module

--- app/src/test/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryViewModelTest.kt
+++ app/src/test/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryViewModelTest.kt
@@ -25,75 +25,75 @@ import org.threeten.bp.LocalDate
 
 class EntryViewModelTest {
 
-    private lateinit var viewModel: EntryViewModel
-
-    private val repository = object : EntryRepository {
-        override suspend fun getEntry(date: LocalDate): Entry? {
-            return Entry(date, "hii there")
-        }
-
-        override suspend fun getEntriesFlow(): Flow<List<Entry>> {
-            return flowOf(listOf(Entry(LocalDate.of(2021, 2, 28), "hii there")))
-        }
-
-        override suspend fun getEntries(): List<Entry> {
-            return listOf(Entry(LocalDate.of(2021, 2, 28), "hii there"))
-        }
-
-        override fun getWrittenDates(): LiveData<List<LocalDate>> {
-            return MutableLiveData(listOf(LocalDate.of(2021, 2, 28)))
-        }
-
-        override suspend fun addEntry(entry: Entry) = Unit
-
-        override suspend fun addEntries(entries: List<Entry>) = Unit
-
-        override fun searchEntries(query: String): Flow<PagingData<Entry>> = flowOf(PagingData.empty())
-    }
-    private val application = mock<Application>()
-
-    @get:Rule
-    val mvrxRule = MvRxTestRule()
-
-    @Before
-    fun before() {
-        whenever(application.resources).thenReturn(mock())
-        whenever(application.resources.getStringArray(anyInt())).thenReturn(arrayOf("InspirationalQuote"))
-    }
-
-    @Test
-    fun `GIVEN entry view model WHEN changePrompt is called THEN the state is updated`() {
-        val initialState = EntryState(LocalDate.now(), "", null, "quote", false, 0, listOf("one", "two"), false)
-        viewModel = EntryViewModel(initialState, repository)
-        viewModel.changePrompt()
-
-        withState(viewModel) {
-            assertEquals(it.promptNumber, 1)
-            assertEquals(it.hint, "two")
-        }
-    }
-
-    @Test
-    fun `GIVEN entry view model WHEN onTextChanged is called THEN the state is updated`() {
-        val initialState = EntryState(LocalDate.now(), "", null, "quote", false, 0, listOf("one", "two"), false)
-        viewModel = EntryViewModel(initialState, repository)
-        viewModel.onTextChanged("new text")
-
-        withState(viewModel) {
-            assertEquals(it.entryContent, "new text")
-            assertEquals(it.hasUserEdits, true)
-        }
-    }
-
-    @Test
-    fun `GIVEN entry view model WHEN setDate is called THEN the state is updated`() {
-        val initialState = EntryState(LocalDate.now(), "", null, "quote", false, 0, listOf("one", "two"), false)
-        viewModel = EntryViewModel(initialState, repository)
-        viewModel.setDate("2021-03-02")
-
-        withState(viewModel) {
-            assertEquals(it.date, LocalDate.of(2021, 3, 2))
-            assertEquals(it.entryContent, "hii there")
-        }
-    }
+//    private lateinit var viewModel: EntryViewModel
+//
+//    private val repository = object : EntryRepository {
+//        override suspend fun getEntry(date: LocalDate): Entry? {
+//            return Entry(date, "hii there")
+//        }
+//
+//        override suspend fun getEntriesFlow(): Flow<List<Entry>> {
+//            return flowOf(listOf(Entry(LocalDate.of(2021, 2, 28), "hii there")))
+//        }
+//
+//        override suspend fun getEntries(): List<Entry> {
+//            return listOf(Entry(LocalDate.of(2021, 2, 28), "hii there"))
+//        }
+//
+//        override fun getWrittenDates(): LiveData<List<LocalDate>> {
+//            return MutableLiveData(listOf(LocalDate.of(2021, 2, 28)))
+//        }
+//
+//        override suspend fun addEntry(entry: Entry) = Unit
+//
+//        override suspend fun addEntries(entries: List<Entry>) = Unit
+//
+//        override fun searchEntries(query: String): Flow<PagingData<Entry>> = flowOf(PagingData.empty())
+//    }
+//    private val application = mock<Application>()
+//
+//    @get:Rule
+//    val mvrxRule = MvRxTestRule()
+//
+//    @Before
+//    fun before() {
+//        whenever(application.resources).thenReturn(mock())
+//        whenever(application.resources.getStringArray(anyInt())).thenReturn(arrayOf("InspirationalQuote"))
+//    }
+//
+//    @Test
+//    fun `GIVEN entry view model WHEN changePrompt is called THEN the state is updated`() {
+//        val initialState = EntryState(LocalDate.now(), "", null, "quote", false, 0, listOf("one", "two"), false)
+//        viewModel = EntryViewModel(initialState, repository)
+//        viewModel.changePrompt()
+//
+//        withState(viewModel) {
+//            assertEquals(it.promptNumber, 1)
+//            assertEquals(it.hint, "two")
+//        }
+//    }
+//
+//    @Test
+//    fun `GIVEN entry view model WHEN onTextChanged is called THEN the state is updated`() {
+//        val initialState = EntryState(LocalDate.now(), "", null, "quote", false, 0, listOf("one", "two"), false)
+//        viewModel = EntryViewModel(initialState, repository)
+//        viewModel.onTextChanged("new text")
+//
+//        withState(viewModel) {
+//            assertEquals(it.entryContent, "new text")
+//            assertEquals(it.hasUserEdits, true)
+//        }
+//    }
+//
+//    @Test
+//    fun `GIVEN entry view model WHEN setDate is called THEN the state is updated`() {
+//        val initialState = EntryState(LocalDate.now(), "", null, "quote", false, 0, listOf("one", "two"), false)
+//        viewModel = EntryViewModel(initialState, repository)
+//        viewModel.setDate("2021-03-02")
+//
+//        withState(viewModel) {
+//            assertEquals(it.date, LocalDate.of(2021, 3, 2))
+//            assertEquals(it.entryContent, "hii there")
+//        }
+//    }
 }
