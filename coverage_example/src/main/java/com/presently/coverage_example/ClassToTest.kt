3e332e1e6055762603d60da27916d8b2ff357ae5
alisonthemonster <alison.wyllie@gmail.com>
2021-06-03 22:15:40 -0700

    Create module to test if code coverage is properly measured

---
+++ coverage_example/src/main/java/com/presently/coverage_example/ClassToTest.kt
@@ -0,0 +1,12 @@
+package com.presently.coverage_example
+
+class ClassToTest {
+
+    fun mathIsGreat(): Int {
+        return 2*4
+    }
+
+    fun wordsAreCool(): String {
+        return "So cool"
+    }
+}
---
+++ coverage_example/src/test/java/com/presently/coverage_example/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.presently.coverage_example
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MyUnitTest {
+    @Test
+    fun mathWorks() {
+        val actual = ClassToTest().mathIsGreat()
+        val expected = 8
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun wordsWork() {
+        val actual = ClassToTest().wordsAreCool()
+        val expected = "So cool"
+        assertEquals(expected, actual)
+    }
+}
