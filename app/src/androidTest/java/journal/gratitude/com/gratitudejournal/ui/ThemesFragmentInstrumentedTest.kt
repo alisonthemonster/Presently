2bcc58628cc39e9a57716a31a6e700ca625c0774
alisonthemonster <alison.wyllie@gmail.com>
2021-07-11 21:53:10 -0700

    Fix tests

--- app/src/androidTest/java/journal/gratitude/com/gratitudejournal/ui/ThemesFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/journal/gratitude/com/gratitudejournal/ui/ThemesFragmentInstrumentedTest.kt
@@ -1,22 +1,28 @@
 package journal.gratitude.com.gratitudejournal.ui
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import journal.gratitude.com.gratitudejournal.R
+import journal.gratitude.com.gratitudejournal.testUtils.launchFragmentInHiltContainer
 import journal.gratitude.com.gratitudejournal.ui.themes.ThemeFragment
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class ThemesFragmentInstrumentedTest {
 
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Test
     fun themes_are_presented() {
-        launchFragmentInContainer<ThemeFragment>(
+        launchFragmentInHiltContainer<ThemeFragment>(
             themeResId = R.style.Base_AppTheme
         )
 
