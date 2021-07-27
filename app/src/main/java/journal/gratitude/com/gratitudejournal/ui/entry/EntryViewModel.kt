5dc1df9951cbafedc465c89053fae254bac84e83
alisonthemonster <alison.wyllie@gmail.com>
2021-07-26 21:24:27 -0700

    Refactor entry screen to use mavericks args, move logic into viewmodel, and write tests

--- app/src/main/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryViewModel.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryViewModel.kt
@@ -2,28 +2,45 @@ package journal.gratitude.com.gratitudejournal.ui.entry
 
 import com.airbnb.mvrx.MavericksViewModel
 import com.airbnb.mvrx.MavericksViewModelFactory
-import com.airbnb.mvrx.ViewModelContext
+import com.presently.logging.AnalyticsLogger
 import com.presently.mavericks_utils.AssistedViewModelFactory
-import com.presently.mavericks_utils.CreateMavericksViewModelComponent
-import com.presently.mavericks_utils.HiltMavericksEntryPoint
+import com.presently.mavericks_utils.hiltMavericksViewModelFactory
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import dagger.hilt.EntryPoints
-import journal.gratitude.com.gratitudejournal.R
+import journal.gratitude.com.gratitudejournal.model.CLICKED_PROMPT
+import journal.gratitude.com.gratitudejournal.model.EDITED_EXISTING_ENTRY
 import journal.gratitude.com.gratitudejournal.model.Entry
+import journal.gratitude.com.gratitudejournal.model.Milestone
 import journal.gratitude.com.gratitudejournal.repository.EntryRepository
-import journal.gratitude.com.gratitudejournal.util.toLocalDate
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.threeten.bp.LocalDate
 
 class EntryViewModel @AssistedInject constructor(
     @Assisted initialState: EntryState,
+    private val analytics: AnalyticsLogger,
     private val repository: EntryRepository
 ) : MavericksViewModel<EntryState>(initialState) {
 
+    init {
+        withState {
+            viewModelScope.launch {
+                val entry = repository.getEntry(it.date)
+                if (entry != null) {
+                    setState {
+                        copy(entryContent = entry.entryContent)
+                    }
+                }
+
+            }
+        }
+    }
+
+    fun onCreate() {
+        analytics.recordView("EntryFragment")
+    }
+
     fun changePrompt() {
+        analytics.recordEvent(CLICKED_PROMPT)
         setState {
             val newPromptNumber = if (promptNumber < promptsList.size - 1) promptNumber + 1 else 0
             val newPrompt = promptsList[newPromptNumber]
@@ -44,53 +61,33 @@ class EntryViewModel @AssistedInject constructor(
     fun saveEntry() {
         withState {
             val entry = Entry(it.date, it.entryContent)
-            viewModelScope.launch(Dispatchers.IO) {
+            viewModelScope.launch {
                 repository.addEntry(entry)
+            }
+            if (it.isNewEntry) {
+                val totalEntries = (it.numberExistingEntries ?: 0) + 1
+                analytics.recordEntryAdded(totalEntries)
+                if (Milestone.milestones.contains(totalEntries)) {
+                    setState {
+                        copy(milestoneNumber = totalEntries)
+                    }
+                }
+            } else {
+                analytics.recordEvent(EDITED_EXISTING_ENTRY)
                 setState {
                     copy(isSaved = true)
                 }
             }
-        }
-    }
 
-    fun setDate(passedInDate: String) {
-        val date = passedInDate.toLocalDate()
-        setState {
-            copy(date = date)
-        }
-        viewModelScope.launch {
-            val entry = repository.getEntry(date)
-            setState {
-                copy(entryContent = entry?.entryContent ?: "")
-            }
         }
     }
 
     @AssistedFactory
-    interface Factory: AssistedViewModelFactory<EntryViewModel, EntryState> {
+    interface Factory : AssistedViewModelFactory<EntryViewModel, EntryState> {
         override fun create(state: EntryState): EntryViewModel
     }
 
-    companion object : MavericksViewModelFactory<EntryViewModel, EntryState> {
+    companion object :
+        MavericksViewModelFactory<EntryViewModel, EntryState> by hiltMavericksViewModelFactory()
 
-        override fun initialState(viewModelContext: ViewModelContext): EntryState {
-            val passedInDate = LocalDate.now() // TODO is there a way get from fragment args?
-            val prompts = viewModelContext.activity.resources.getStringArray(R.array.prompts)
-            prompts.shuffle() //randomise prompts
-            val quote = viewModelContext.activity.resources.getStringArray(R.array.inspirations).random()
-            return EntryState(passedInDate, "", null, quote, false, 0, prompts.toList(), false)
-        }
-
-        override fun create(viewModelContext: ViewModelContext, state: EntryState): EntryViewModel {
-            // We want to create the ViewModelComponent. In order to do that, we need to get its parent: ActivityComponent.
-            val componentBuilder = EntryPoints.get(viewModelContext.app(), CreateMavericksViewModelComponent::class.java).mavericksViewModelComponentBuilder()
-            val viewModelComponent = componentBuilder.build()
-            val viewModelFactoryMap = EntryPoints.get(viewModelComponent, HiltMavericksEntryPoint::class.java).viewModelFactories
-            val viewModelFactory = viewModelFactoryMap[EntryViewModel::class.java]
-
-            @Suppress("UNCHECKED_CAST")
-            val castedViewModelFactory = viewModelFactory as? AssistedViewModelFactory<EntryViewModel, EntryState>
-            return castedViewModelFactory?.create(state)!!
-        }
-    }
 }
