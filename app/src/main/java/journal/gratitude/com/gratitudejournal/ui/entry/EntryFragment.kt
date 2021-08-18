74381345ededec981fc1bb82cfed4643e743fbb2
alisonthemonster <alison.wyllie@gmail.com>
2021-08-17 21:24:57 -0700

    Update credential logic in entry fragment and add more tests for settings

--- app/src/main/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryFragment.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/ui/entry/EntryFragment.kt
@@ -223,10 +223,9 @@ class EntryFragment : Fragment(), MavericksView {
 
     private fun backupEntryIfNeeded() {
         val dbxCredential = settings.getAccessToken()
-        val accessToken: DbxCredential? = if (dbxCredential == "attempted") null else DbxCredential.Reader.readFully(dbxCredential)
 
         val cadence = settings.getAutomaticBackupCadence()
-        if (accessToken != null && cadence == BackupCadence.EVERY_CHANGE) {
+        if (dbxCredential != null && cadence == BackupCadence.EVERY_CHANGE) {
             val uploadWorkRequest = OneTimeWorkRequestBuilder<UploadToCloudWorker>()
                 .addTag(DropboxUploader.PRESENTLY_BACKUP)
                 .build()
