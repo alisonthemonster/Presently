f6debb2056af11c571515d69e09914e6bd61088a
alisonthemonster <alison.wyllie@gmail.com>
2021-10-06 11:33:10 -0700

    Update UI build.gradle to build.gradle.kts

---
+++ ui/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = Versions.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+
+    dependencies {
+        implementation(Libraries.kotlin_stdlib)
+        implementation(Libraries.androidx_core_ktx)
+    }
+}
