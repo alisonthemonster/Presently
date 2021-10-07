25a7592ac556b9e2ec12d310988f8eabfde5aa17
alisonthemonster <alison.wyllie@gmail.com>
2021-10-06 17:40:36 -0700

    Update Sharing build.gradle to build.gradle.kts

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,12 +5,23 @@ object Versions {
 
     const val KOTLIN = "1.4.32" //https://developer.android.com/jetpack/androidx/releases/core
     const val ANDROIDX_CORE_KTX = "1.6.0" //https://developer.android.com/jetpack/androidx/releases/core
+    const val ANDROIDX_COMPAT = "1.3.0"
+    const val ANDROIDX_CONSTRAINT = "2.0.4"
+    const val ANDROIDX_LIFECYCLE_EXT = "2.2.0"
+    const val ANDROIDX_LIFECYCLE = "2.3.1"
+    const val ANDROIDX_RECYCLER_VIEW = "1.2.0"
+    const val FRAGMENT_TESTING = "1.3.4"
+    const val ANDROIDX_TEST_JUNIT = "1.1.2"
+    const val ANDROIDX_TEST_CORE_KTX = "1.3.0"
+    const val ANDROIDX_TEST_ESPRESSO = "3.3.0"
+    const val ANDROIDX_TEST_RULES = "1.3.0"
     const val FIREBASE = "28.2.1" //https://firebase.google.com/support/release-notes/android
     const val DAGGER = "2.38.1" //https://github.com/google/dagger/releases
     const val ASSISTED_INJECT = "0.3.2" //TODO use dagger's version: https://dagger.dev/dev-guide/assisted-injection.html
     const val HILT_ANDROID = "1.0.0-alpha03" //https://developer.android.com/jetpack/androidx/releases/hilt
     const val JUNIT = "4.13.2"
     const val MOCKITO_KOTLIN = "2.0.0"
+    const val MOCKITO_ANDROID = "2.23.0"
     const val ROBOLECTRIC = "4.6.1" //https://github.com/robolectric/robolectric/releases/
     const val TRUTH = "1.1.3"
     const val MAVERICKS = "2.2.0" //https://github.com/airbnb/mavericks/releases
@@ -34,11 +45,27 @@ object Libraries {
    const val assisted_inject_processor = "com.squareup.inject:assisted-inject-processor-dagger2:${Versions.ASSISTED_INJECT}"
    const val mavericks = "com.airbnb.android:mavericks:${Versions.MAVERICKS}"
    const val mavericks_mocking = "com.airbnb.android:mavericks-mocking:${Versions.MAVERICKS}"
+   const val androidx_compat = "androidx.appcompat:appcompat:${Versions.ANDROIDX_COMPAT}"
+   const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.ANDROIDX_CONSTRAINT}"
+   const val androidx_lifecycle_ext = "androidx.lifecycle:lifecycle-extensions:${Versions.ANDROIDX_LIFECYCLE_EXT}"
+   const val androidx_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.ANDROIDX_LIFECYCLE}"
+   const val androidx_lifecycle_runtime_ktx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.ANDROIDX_LIFECYCLE}"
+   const val androidx_recycler_view = "androidx.recyclerview:recyclerview:${Versions.ANDROIDX_RECYCLER_VIEW}"
 }
 
 object TestLibraries {
     const val junit = "junit:junit:${Versions.JUNIT}"
     const val mockito_kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.MOCKITO_KOTLIN}"
+    const val mockito_android = "org.mockito:mockito-android::${Versions.MOCKITO_ANDROID}"
     const val robolectric = "org.robolectric:robolectric:${Versions.ROBOLECTRIC}"
     const val truth = "com.google.truth:truth:${Versions.TRUTH}"
+    const val fragment_testing = "androidx.fragment:fragment-testing:${Versions.FRAGMENT_TESTING}"
+    const val mavericks_testing = "com.airbnb.android:mavericks-testing:${Versions.MAVERICKS}"
+    const val androidx_test_junit = "androidx.test.ext:junit:${Versions.ANDROIDX_TEST_JUNIT}"
+    const val androidx_test_core_ktx = "androidx.test:core-ktx:${Versions.ANDROIDX_TEST_CORE_KTX}"
+    const val androidx_test_espresso_core = "androidx.test.espresso:espresso-core:${Versions.ANDROIDX_TEST_ESPRESSO}"
+    const val androidx_test_espresso_contrib = "androidx.test.espresso:espresso-contrib:${Versions.ANDROIDX_TEST_ESPRESSO}"
+    const val androidx_test_espresso_intents = "androidx.test.espresso:espresso-intents:${Versions.ANDROIDX_TEST_ESPRESSO}"
+    const val androidx_test_rules = "androidx.test.rules:${Versions.ANDROIDX_TEST_RULES}"
+    const val hilt_android_testing = "hilt-android-testing::${Versions.DAGGER}"
 }
---
+++ sharing/build.gradle.kts
@@ -0,0 +1,99 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-android-extensions")
+    id("com.facebook.testing.screenshot")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = Versions.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isTestCoverageEnabled = project.hasProperty("coverage")
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+    packagingOptions {
+        exclude 'META-INF/AL2.0'
+        exclude 'META-INF/LGPL2.1'
+    }
+}
+
+dependencies {
+    implementation(project(':ui'))
+    implementation(project(':mavericks_utils'))
+    implementation(project(':strings'))
+    implementation(project(':logging'))
+
+//    implementation fileTree(dir: "libs", include: ["*.jar"])
+    implementation(Libraries.kotlin_stdlib)
+    implementation(Libraries.androidx_core_ktx)
+    implementation(Libraries.androidx_compat)
+    implementation(Libraries.androidx_constraint_layout)
+    implementation(Libraries.androidx_lifecycle_ext)
+    implementation(Libraries.androidx_viewmodel_ktx)
+    implementation(Libraries.androidx_lifecycle_runtime_ktx)
+    implementation(Libraries.androidx_recycler_view)
+//    implementation 'com.google.android.material:material:1.3.0'
+
+    implementation(Libraries.mavericks)
+    implementation(Libraries.mavericks_mocking)
+//    annotationProcessor 'com.airbnb.android:epoxy-processor:4.0.0-beta6'
+
+    //dependency injection
+    implementation(Libraries.dagger)
+    kapt(Libraries.dagger_compiler)
+    implementation(Libraries.dagger_android_support)
+    kapt(Libraries.dagger_android_processor)
+    compileOnly(Libraries.assisted_inject_annotations)
+    kapt(Libraries.assisted_inject_processor)
+    implementation(Libraries.hilt)
+    kapt(Libraries.hilt_compiler)
+    implementation(Libraries.hilt_viewmodel)
+    kapt(Libraries.hilt_android_compiler)
+
+    debugImplementation(TestLibraries.fragment_testing)
+
+    testImplementation(TestLibraries.junit)
+    testImplementation(TestLibraries.mavericks_testing)
+    testImplementation(TestLibraries.truth)
+
+    debugImplementation(project(':testing')) //needed for the hilt test activity
+    androidTestImplementation(TestLibraries.androidx_test_junit)
+    androidTestImplementation(TestLibraries.androidx_test_core_ktx)
+    androidTestImplementation(TestLibraries.androidx_test_espresso_core)
+    androidTestImplementation(TestLibraries.androidx_test_espresso_contrib)
+    androidTestImplementation(TestLibraries.androidx_test_espresso_intents)
+    androidTestImplementation(TestLibraries.androidx_test_rules)
+    androidTestImplementation(TestLibraries.mavericks_testing)
+    androidTestImplementation(TestLibraries.mockito_android)
+    androidTestImplementation(TestLibraries.hilt_android_testing)
+    kaptAndroidTest(Libraries.hilt_compiler)
+}
