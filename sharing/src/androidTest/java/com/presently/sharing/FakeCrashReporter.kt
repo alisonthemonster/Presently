fe5310c57c25e0f0ea2f71fde8481881ab873b44
alisonthemonster <alison.wyllie@gmail.com>
2021-12-12 19:56:54 -0800

    Add fake for tests

---
+++ sharing/src/androidTest/java/com/presently/sharing/FakeCrashReporter.kt
@@ -0,0 +1,34 @@
+package com.presently.sharing
+
+import com.presently.logging.CrashReporter
+import com.presently.logging.wiring.AnalyticsModule
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import java.lang.Exception
+import javax.inject.Singleton
+
+class FakeCrashReporter: CrashReporter {
+    override fun logHandledException(exception: Exception) {}
+
+    override fun optOutOfCrashReporting() {}
+
+    override fun optIntoCrashReporting() {}
+}
+
+/**
+ * CrashReporter binding to use in instrumented tests.
+ *
+ * Hilt will inject a [FakeCrashReporter] instead of a [RealCrashReporter].
+ */
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [AnalyticsModule::class]
+)
+abstract class FakeAnalyticsModule {
+    @Singleton
+    @Binds
+    abstract fun bindCrashReporter(crashReporter: FakeCrashReporter): CrashReporter
+}
