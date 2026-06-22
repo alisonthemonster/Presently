e95a84616933ba7808f82f396e67f7b047d4095d
Riley Grigg <griggriley@gmail.com>
2026-06-22 11:32:55 -0600

    Prep for merge

--- app/src/main/java/journal/gratitude/com/gratitudejournal/ui/security/LockingService.kt
+++ app/src/main/java/journal/gratitude/com/gratitudejournal/ui/security/LockingService.kt
@@ -20,7 +20,7 @@ class LockingService : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return START_STICKY
+        return START_NOT_STICKY
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
